5d4d8937033fdf9148d2a6d3fd9a4a0ceaa8d816
Danielle Rangel <daniellerangel1912@gmail.com>
2021-06-11 20:48:30 -0300

    alteracao repositories

--- Codigos/sa-backend-spring/src/main/java/edu/ifes/ci/si/les/sa/repositories/AulaRepository.java
+++ Codigos/sa-backend-spring/src/main/java/edu/ifes/ci/si/les/sa/repositories/AulaRepository.java
@@ -1,8 +1,11 @@
 package edu.ifes.ci.si.les.sa.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import edu.ifes.ci.si.les.sa.model.Aula;
+
 @Repository
-public interface AulaRepository{
+public interface AulaRepository extends JpaRepository<Aula, Integer>{
 
 }
