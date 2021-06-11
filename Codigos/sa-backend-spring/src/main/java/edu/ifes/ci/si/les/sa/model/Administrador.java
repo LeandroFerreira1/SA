9ad6a6a9e105ffaaa27de470fd8f96ebc3223372
Leandro Ferreira <leandroferreira281@gmail.com>
2021-06-10 22:43:54 -0300

    modificacao classe pessoa e adm

--- Codigos/sa-backend-spring/src/main/java/edu/ifes/ci/si/les/sa/model/Administrador.java
+++ Codigos/sa-backend-spring/src/main/java/edu/ifes/ci/si/les/sa/model/Administrador.java
@@ -1,12 +1,29 @@
 package edu.ifes.ci.si.les.sa.model;
 
 import java.io.Serializable;
+import java.util.Date;
 
-public class Administrador extends Pessoa implements Serializable{
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import lombok.*;
+
+import org.hibernate.annotations.Entity;
+
+
+@Entity 
+@Data 
+@AllArgsConstructor 
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class Administrador extends Pessoa {
+	
 	private static final long serialVersionUID = 1L;
-
+	
 	private String email;
-
+	
+	@Column(length = 20)
+    @NotBlank(message = "Senha do Administrador deve ser preenchida")
+    @Size(min = 6, max = 10, message = "Senha do Administrador deve ter entre 6 e 20 caracteres")
 	private String senha;
 
 }
